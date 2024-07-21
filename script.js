37a9ad180332c104b787ad61dacc626227026373
Amanda Amendoeira <145858289+amandaamendoeira@users.noreply.github.com>
2024-07-21 00:11:08 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.cabecalho__menu-toggle');
-    const menu = document.querySelector('.cabecalho__menu');
-  
-    toggleButton.addEventListener('click', function () {
-      menu.classList.toggle('cabecalho__menu--active');
-    });
-  });
-  
